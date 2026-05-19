c3d4931b000b7177a715d18287757df9e9a3f0d1
2025sl93098-ooviya <2025sl93098@wilp.bits-pilani.ac.in>
2026-05-19 18:00:39 +0530

    Add docker for labsheet3

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.11-slim 
+WORKDIR /app 
+COPY calculator.py . 
+CMD ["python", "calculator.py"] 
--- calculator.py
+++ calculator.py
@@ -8,3 +8,11 @@ def divide(a,b):
     if b==0:
        return None
     return a/b
+
+if __name__ == "__main__":
+      print("Calculator Application")
+      print("add(10, 5) =", add(10, 5))
+      print("subtract(10, 5) =", subtract(10, 5))
+      print("multiply(10, 5) =", multiply(10, 5))
+      print("divide(10, 5) =", divide(10, 5))
+      print("divide(10, 0) =", divide(10, 0))
